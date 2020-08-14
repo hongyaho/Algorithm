45eeae62f556009a0dbede7b43f965541af78924
Youngju <yj8638@gmail.com>
2020-08-14 16:50:21 +0900

    Got it!!

--- ALtudy_Java/src/BOJ_9012.java
+++ ALtudy_Java/src/BOJ_9012.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 import java.util.Stack;
 
 // 괄호 짝 맞는지
-// ##1)  왜 n-1회 돌아가니,,,,,
-// ##2)  몇번할껀지만 쳐도 NO 나옴(size 0이면 출력돼서)
+// ##1)  왜 n-1회 돌아가니,,,,, : 9093번과 같은 이유 (intNext 와 nextLine 같이 써서)
+// ##2)  몇번할껀지만 쳐도 NO 나옴(size 0이면 출력돼서)  : \##1 해결했더니 해결됨
+//            (##1 때문에 input에 빈 문자열이 들어감 -> ""은 (,) 둘 다 해당 안되니까 size 값 변화 없는채로(size=0) 내려가서 YES 출력된거였음
 public class BOJ_9012 {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
+        int num = Integer.parseInt(scanner.nextLine());  //*** 원래 코드 : int num = scanner.nextInt();
 
 
         while (num != 0) {
@@ -24,7 +25,7 @@ public class BOJ_9012 {
                     }
                 } else if (uinput.charAt(i) == ')') {
                     stack.push(uinput.charAt(i));
-                    size++;
+                    size++;  //*****(line 23)
                 }
 
             }
--- ALtudy_Java/src/BOJ_9093.java
+++ ALtudy_Java/src/BOJ_9093.java
@@ -5,12 +5,17 @@ public class BOJ_9093 {
 
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
+        int num = Integer.parseInt(scanner.nextLine().charAt(0)+""); //*** 원래 코드 : int num = nextInt()
 
-        while (num != 0) {
+        /* 문제 : while문이 n-1번 돌아갔음 + for문 건너뛰고 개행 들어감 (line 33)
+        // --> WHY? nextInt 와 nextLine 같이 써서 n뒤에 있는 '\n'(빈 문자열 "")이 input에 들어감
+        //          문자를 여러줄 받는 경우, nextLine 메서드를 이용해서 한 줄 씩 읽어서 처리하자.
+        //              ※ next() 개행문자를 무시하고 입력을 받음, 즉 숫자를 치고 엔터를 누를경우 엔터 전까지만 입력을 받음.
+        //              ※ nextLine() 한줄 단위로 입력 받기 때문에, 개행문자도 한 줄로 인식합니다.
+        */
+        while ((num--) != 0) {    // 원래 코드 : while (num != 0) { num--; ~~~
             Stack stack = new Stack<>();
             String uinput = scanner.nextLine()+" ";
-            num--;
 
             for (int j = 0; j < uinput.length(); j++) {
                 // 띄어쓰기(빈 문자열) 있으면 출력
