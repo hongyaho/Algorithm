3da46c9ddd17d135f235c3ef75f198d28ed501fe
Youngju <yj8638@gmail.com>
2020-08-18 15:33:21 +0900

    소수 찾기

--- ALtudy_Java/src/Math/boj_1978.java
+++ ALtudy_Java/src/Math/boj_1978.java
@@ -3,13 +3,14 @@ package Math;
 import java.util.Scanner;
 
 // 소수 찾기
+// 소수 : 1과 자기 자신만으로 나누어 떨어지는 1보다 큰 양의 정수
 public class boj_1978 {
     public static boolean isPrime(int uinput) {
-        //boolean isPrime = true;
 
         if (uinput < 2)
             return false;
 
+        // 1과 자기 자신만으로 나누어 떨어져야 하니까 2부터 시작, 자기 자신 전까지만 반복
         for (int i = 2; i < uinput; i++) {
             if (uinput % i == 0) {
                 return false;
